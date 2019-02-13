af70b0187e418ae5781fbc6315b82f35f085d629
Jayaram Mani <jayaram.mani@optum.com>
2019-02-13 12:04:12 +0000

    cycletowork bike api update

--- bike/src/main/java/com/cycletowork/bike/controllers/BikesController.java
+++ bike/src/main/java/com/cycletowork/bike/controllers/BikesController.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import com.cycletowork.bike.models.Bike;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,10 +23,13 @@ public class BikesController {
         return bikes;
     }
 
+    @PostMapping
+    @ResponseStatus(HttpStatus.OK)
     public void create(@RequestBody Bike bike) {
 
     }
 
+    @GetMapping("/{id}")
     public Bike get(@PathVariable("id") long id) {
         return new Bike();
     }
